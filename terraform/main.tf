4a976e3e615cfcf99c1b58bdebc48fe5328979d7
Melody Egwuchukwu <melodyegwuchukwu1@gmail.com>
2025-10-30 15:35:24 +0100

    terraform s3, eventbridge provision

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,8 +11,17 @@ provider "aws" {
   region = "eu-central-1"
 }
 
-resource "aws_kinesis_stream" "device_stream" {
-  name             = "device-stream"
-  shard_count      = 1
-  retention_period = 24
+resource "aws_s3_bucket" "device_events" {
+  bucket = "device-events-serveless-project"
+}
+
+resource "aws_cloudwatch_event_rule" "device_rule" {
+  name        = "device-event-rule"
+  description = "Rule to trigger Lambda from S3 events"
+  event_pattern = <<PATTERN
+{
+  "source": ["aws.s3"],
+  "detail-type": ["Object Created"]
+}
+PATTERN
 }
